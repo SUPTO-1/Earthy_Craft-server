a38e18df2837333ba07069564d7bc04b0e0f0bf7
SUPTO-1 <jahidulislamsupta9@gmail.com>
2024-04-28 20:10:45 +0600

    search with id added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -36,7 +36,12 @@ async function run() {
       res.send(result);
     })
 
-
+    app.get('/crafts/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await craftsCollection.findOne(query);
+      res.send(result);
+    })
 
     app.post('/crafts',async(req,res)=>{
         const newCrafts = req.body;
