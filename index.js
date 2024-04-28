942a462348bf03e8f79f92e67cb8aa03e90f92fd
SUPTO-1 <jahidulislamsupta9@gmail.com>
2024-04-28 12:33:19 +0600

    newdata is hitting on the server

--- index.js
+++ index.js
@@ -26,6 +26,14 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const craftsCollection = client.db("earthyCraft").collection("crafts");
+    app.post('/crafts',async(req,res)=>{
+        const newCrafts = req.body;
+        console.log(newCrafts);
+        const result = await craftsCollection.insertOne(newCrafts);
+        res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
