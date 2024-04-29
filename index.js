7ecb465b67642c1b3853b25868c673faad7253a2
SUPTO-1 <jahidulislamsupta9@gmail.com>
2024-04-29 10:16:59 +0600

    added subcategory to the database collection

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ async function run() {
 
     
     const craftsCollection = client.db("earthyCraft").collection("crafts");
-
+    const subcategoryCollection = client.db("earthyCraft").collection("subcategory");
     app.get('/crafts',async(req, res)=>{
       const cursor = craftsCollection.find();
       const result = await cursor.toArray();
@@ -85,6 +85,12 @@ async function run() {
       const result = await craftsCollection.deleteOne(query);
       res.send(result);
     })
+
+    app.get('/subcategory',async(req, res)=>{
+      const cursor = subcategoryCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+  })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
